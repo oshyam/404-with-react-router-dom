de7a66c0251553f7e0628dae055025f156b9adde
Sam <myworkplaceservice@gmail.com>
2021-07-19 23:51:05 +0530

    404 Made with react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+//React-Router DOM
+import {BrowserRouter as Router, Route,Switch,Link} from "react-router-dom"
+
+//Components
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
+
+
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+       <div>
+    <ul>
+      <li><Link to="/Home">Back to Home</Link></li>
+      <li><Link to="/About">About</Link></li>
+      <li><Link to="/Contact">Contact</Link></li>
+
+    </ul>
     </div>
-  );
+
+
+    <Switch>
+    <Route exact path="/Home" component={Home}/>
+    <Route exact path="/About" component={About}/>
+    <Route exact path="/Contact" component={Contact}/>
+    <Route exact path="/*" component={PageNotFound}/>
+
+    </Switch>
+  </Router>
+  )
 }
 
 export default App;
